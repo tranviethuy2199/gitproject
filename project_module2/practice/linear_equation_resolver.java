214af223a0418ad4b752557c7f33cf9fc14fa15a
tranviethuy2199 <huytran211998@gmail.com>
2022-08-01 15:27:57 +0700

    nop bai tap

---
+++ project_module2/practice/linear_equation_resolver.java
@@ -0,0 +1,31 @@
+package practice;
+
+import java.util.Scanner;
+
+public class linear_equation_resolver {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a*x+b=c,please enter constants ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a:");
+        double a = scanner.nextDouble();
+
+        System.out.println("b:");
+        double b = scanner.nextDouble();
+
+        System.out.println("c:");
+        double c = scanner.nextDouble();
+
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.println("Equation pass width x = %f!\n");
+        } else {
+            if (b == c) {
+                System.out.println("the solution is all x!");
+            } else {
+                System.out.println("No solution");
+            }
+
+        }
+    }
+}
