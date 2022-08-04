6e36d1c26b6b83d02faf6e72e1d1de5099f11f5b
tranviethuy2199 <huytran211998@gmail.com>
2022-08-04 18:44:02 +0700

    nop bai tap

--- git_clone_thuc_hanh/project_module2/src/ss4_class_and_object/homeWork/Fan/Fan.java
+++ git_clone_thuc_hanh/project_module2/src/ss4_class_and_object/homeWork/Fan/Fan.java
@@ -1,21 +1,21 @@
 package ss4_class_and_object.homeWork.Fan;
 
 public class Fan {
-//    int speed, radius, color, boolean button;
-    private int speed ;
-    private boolean button ;
-    private double radius ;
-    private String color ;
+    //    int speed, radius, color, boolean button;
+    private int speed;
+    private boolean button;
+    private double radius;
+    private String color;
 
-   public Fan (){
+       public Fan (){
        this.speed = 1;
        this.button = false;
        this.radius = 5;
-       this.String = yellow;
+       this.color = "yellow";
    }
-    public Fan(int speed, boolean on, double radius, String color) {
+    public Fan(int speed, boolean button, double radius, String color) {
         this.speed = speed;
-        this.button = on;
+        this.button = false;
         this.radius = radius;
         this.color = color;
     }
@@ -28,7 +28,47 @@ public class Fan {
         this.speed = speed;
     }
 
+    public boolean getButton() {
+        return button;
+    }
 
+    public void setButton(boolean button) {
+        this.button = button;
+    }
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        if (this.getButton()) {
+            return "Fan{" +
+                    "speed=" + this.getSpeed() +
+                    ", radius=" + this.getRadius() +
+                    ", color='" + this.getColor() + '\'' +
+                    "fan is on" +
+                    '}';
+        } return "Fan{" +
+                "speed=" + this.getSpeed() +
+                ", radius=" + this.getRadius() +
+                ", color='" + this.getColor() + '\'' +
+                "fan is off" +
+                '}';
+    }
 
 }
+
+
--- git_clone_thuc_hanh/project_module2/src/ss4_class_and_object/homeWork/Fan/Main.java
+++ git_clone_thuc_hanh/project_module2/src/ss4_class_and_object/homeWork/Fan/Main.java
@@ -1,4 +1,27 @@
 package ss4_class_and_object.homeWork.Fan;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+
 public class Main {
+//    Fan fan1 = new Fan();
+//    Fan fan2 = new Fan(9, true, 30, "blue");
+public static void main(String[] args) {
+    Fan fan1 = new Fan();
+    fan1.setSpeed(10);
+    fan1.setButton(true);
+    fan1.setRadius(5);
+    System.out.println(fan1.toString());
+
+    Fan fan2 = new Fan();
+    fan2.setButton(false);
+    fan2.setRadius(5);
+    fan2.setSpeed(5);
+    fan2.setColor("blue");
+    System.out.println(fan2.toString());
+
 }
+
+}
+
